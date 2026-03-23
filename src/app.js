1c2f21d2dc28e89e50b5bdef1c4e7f21ebabc6ef
Doc <4009206+doccerz@users.noreply.github.com>
2026-03-23 07:06:00 +0000

    feat: register @fastify/swagger and @fastify/swagger-ui at /docs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import Fastify from 'fastify'
 import fastifyEnv from '@fastify/env'
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 import { envSchema } from './config/env.schema.js'
+import { swaggerConfig } from './docs/openapi.js'
 
 export function buildApp(opts = {}, envData = null) {
   const app = Fastify(opts)
+
+  app.register(swagger, swaggerConfig)
+  app.register(swaggerUi, { routePrefix: '/docs' })
 
   app.register(fastifyEnv, {
     schema: envSchema,
